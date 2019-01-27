ac45be2e01d6338cbe0c461bff4a3e95807dc443
Taran Thamrait <Taran@Tarans-MacBook-Pro.local>
2019-01-27 12:15:10 -0800

    part2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Taran Thamrait on 1/27/19.
+//  Copyright © 2019 Tarandeep. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
